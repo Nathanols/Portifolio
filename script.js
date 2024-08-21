c441a3f80bfba5272bd0e8c7b11eeeed54991c0b
Nathanols <nathanols805@gmail.com>
2024-08-21 00:01:42 -0300

    update

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+
+const mode = document.getElementById('mode_icon');
+
+mode.addEventListener('click', () => {
+    if(mode.classList.contains('fa-moon')){
+        mode.classList.remove('fa-moon');
+        mode.classList.add('fa-sun');
+
+        return;
+    }
+
+    mode.classList.add('fa-moon');
+    mode.classList.remove('fa-sun');
+});
+
 function menuShow() {
     let menu = document.querySelector('.list');
     if(menu.classList.contains('open')){
